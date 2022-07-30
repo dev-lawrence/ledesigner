fdb9dfac78071e097fa58f4e5b3967e62779e60e
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2022-07-30 20:06:44 +0100

    removed and added some files and finished the nav bar

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,26 @@
-console.log('HELLO');
+const toggle = document.querySelector('.toggle-menu');
+const navMenu = document.querySelector('.nav');
+const navList = document.querySelector('.nav__list');
+const navLinks = navList.querySelectorAll('a');
 
-const test = () => {
-	console.log('this is a test');
+// function to show the Navigation
+const showNav = () => {
+  navMenu.classList.toggle('showMenu');
+  navList.classList.toggle('appear');
+  // create var for the close icon
+  const close = toggle.querySelector('i');
+  close.classList.toggle('closeIcon');
 };
+
+// Function to change active link on click
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    const active = document.querySelector('.active');
+    active.classList.remove('active');
+    link.classList.add('active');
+
+    showNav();
+  });
+});
+
+toggle.addEventListener('click', showNav);

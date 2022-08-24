4758a301ffb4f066c7eeba9402fa90adcb5b1668
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2022-08-24 13:44:31 +0100

    finished teh featured section

--- app/js/script.js
+++ app/js/script.js
@@ -2,6 +2,7 @@ const toggle = document.querySelector('.toggle-menu');
 const navMenu = document.querySelector('.nav');
 const navList = document.querySelector('.nav__list');
 const navLinks = document.querySelectorAll('.nav__link');
+const loadMore = document.querySelector('#load-more');
 
 // function to show the Navigation
 const showNav = () => {
@@ -27,3 +28,26 @@ const changeActiveLink = (link) => {
 navLinks.forEach(changeActiveLink);
 
 toggle.addEventListener('click', showNav);
+
+// Load more button for featured section
+let currentItem = 3;
+
+const showMore = () => {
+  const featuredCard = document.querySelector('.featured__card');
+  const cards = Array.from(featuredCard.children);
+  cards.forEach((card, index) => {
+    index = currentItem;
+
+    if (index < currentItem + 3) {
+      card.style.display = 'block';
+    }
+
+    currentItem += 3;
+
+    if (currentItem >= cards.length) {
+      loadMore.style.display = 'none';
+    }
+  });
+};
+
+loadMore.addEventListener('click', showMore);

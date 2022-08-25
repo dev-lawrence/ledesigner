3701432f57f3bd1fb6c27531fd1b3614d04f35c6
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2022-08-25 08:39:01 +0100

    added some images and finished another section

--- app/js/script.js
+++ app/js/script.js
@@ -3,8 +3,11 @@ const navMenu = document.querySelector('.nav');
 const navList = document.querySelector('.nav__list');
 const navLinks = document.querySelectorAll('.nav__link');
 const loadMore = document.querySelector('#load-more');
+const carouselSlide = document.querySelector('.testimonial__cards');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
 
-// function to show the Navigation
+// FUNCTION TO SHOW THE NAVIGATION
 const showNav = () => {
   navMenu.classList.toggle('showMenu');
   navList.classList.toggle('appear');
@@ -13,7 +16,7 @@ const showNav = () => {
   close.classList.toggle('closeIcon');
 };
 
-// function to change active link
+// FUNCTION TO CHANGE ACTIVE LINK
 const changeActiveLink = (link) => {
   link.addEventListener('click', () => {
     const active = document.querySelector('.active');
@@ -24,12 +27,12 @@ const changeActiveLink = (link) => {
   });
 };
 
-// Nav links
+// NAV LINKS
 navLinks.forEach(changeActiveLink);
 
 toggle.addEventListener('click', showNav);
 
-// Load more button for featured section
+// LOAD MORE BUTTON FOR FEATURED SECTION
 let currentItem = 3;
 
 const showMore = () => {
@@ -51,3 +54,41 @@ const showMore = () => {
 };
 
 loadMore.addEventListener('click', showMore);
+
+// TESTIMONIAL SLIDER
+let currentSlide = 0;
+const carouselCard = document.querySelector('.testimonial__card');
+const size = carouselCard.getBoundingClientRect().width;
+
+const moveSlide = () => {
+  carouselSlide.style.transform = `translateX(-${currentSlide * size}px)`;
+};
+
+const nextSlide = () => {
+  if (currentSlide < 2) {
+    currentSlide++;
+  }
+
+  // set slide to end
+  else {
+    currentSlide = 2;
+  }
+
+  moveSlide();
+};
+
+const prevSlide = () => {
+  if (currentSlide > 0) {
+    currentSlide--;
+  }
+
+  // set slide to end
+  else {
+    currentSlide = 0;
+  }
+
+  moveSlide();
+};
+
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
